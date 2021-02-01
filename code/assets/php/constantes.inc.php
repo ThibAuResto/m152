174da1c2144a410b8cca3cb601dd2d7016c7b576
thib <thibault.cpt@eduge.ch>
2021-02-01 11:34:04 +0100

    Update 01.02.2021

---
+++ code/assets/php/constantes.inc.php
@@ -0,0 +1,8 @@
+<?php
+// Project    :   Notes
+// Desc.     :   All of constant for the connection to the database
+define("DB_NAME", "notes");
+define("DB_USER", "user_notes");
+define("DB_PASS", "yP5Ma2b6qdf2C3xYzSp5C5heZI3uZedSPfIpTyiTiuEUhoY7O6");
+define("DB_HOST", "127.0.0.1");
+define("DB_CHARSET", "utf8");
